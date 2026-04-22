a377f3619c9673e98d7fea5d0d07c80b8745be33
devkkd <developmentkontentkraftdigital.com>
2026-04-22 16:08:33 +0530

    le

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,9 +35,6 @@ app.use(cors({
   },
   credentials: true,
 }));
-  },
-  credentials: true,
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
